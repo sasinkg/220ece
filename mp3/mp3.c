7015e3f2810ff36a5915c58692beaf333d0b7575
sasinkg2 <sasinkg2@illinois.edu>
2020-09-23 02:10:30 -0500

    Finished

--- mp3/mp3.c
+++ mp3/mp3.c
@@ -1,14 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+/*  partners: dhruvv2, akshay5 
+
+    this lab wasn't too hard because we just had to implement the equation that we received in the instructions. */
 
 int main()
 {
-  int row;
+    int row;
+    
+    printf("Enter a row index: "); /* output to user */
+    scanf("%d", &row); /* input number */
+    unsigned long coeff; /* define coeff which determins the contents of the row */
+    int n = row; /* define row */
+    int k = 0; /* row counter */
+    while(k<n+1) 
+    {
+            coeff = 1;
+            printf("%ld ", coeff);
+            k++;
+            
+            for(int i = 1; i<n+1; i++)
+            {
+	      coeff *=(n+1-k); /* equation for coeff */
+                coeff /=k;
+                printf("%ld ", coeff);
+                k++;
+            }
+            
+    }
 
-  printf("Enter a row index: ");
-  scanf("%d",&row);
-
-  // Write your code here
-
-  return 0;
+    return 0;
 }
+
+
