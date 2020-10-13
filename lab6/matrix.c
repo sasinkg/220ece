827288dfad7ca67ddb54921b54c356c02bc2a650
sasinkg2 <sasinkg2@illinois.edu>
2020-10-12 19:23:02 -0700

    tried lab6

--- lab6/matrix.c
+++ lab6/matrix.c
@@ -6,8 +6,21 @@
  *        matrixB, a one dimensional double array of size k*n
  * OUTPUT: matrixC, a one dimensional double array of size m*n
  */
-void matrix_multiply(double *matrixA,double *matrixB,double *matrixC,int m,int k,int n)
+void matrix_multiply(double *matrixA, double *matrixB, double *matrixC,int m,int k,int n)
 {
 //YOUR CODE HERE
+for (int i = 0; i < m; ++i) {
+      for (int j = 0; j < n; ++j) {
+         matrixC[i][j] = 0;
+      }
+   }
+
+for (int i = 0; i < m; ++i) {
+    for (int a = 0; a < k; ++a) {
+        for (int o = 0; o < n; ++o) {
+            matrixC[i][a] += matrixA[i][o] * matrixB[o][a];
+        }
+    }
+}
 }
 
--- lab6/matrix.h
+++ lab6/matrix.h
@@ -1,2 +1,1 @@
-void matrix_multiply(double *matrixA,double *matrixB,double *matrixC,
-                    int m,int k,int n);
+void matrix_multiply(double *matrixA,double *matrixB,double *matrixC, int m,int k,int n);
