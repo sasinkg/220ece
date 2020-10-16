beed87e0dc42b2e19d4958de31b9e45df3d6d4c2
sasinkg2 <sasinkg2@illinois.edu>
2020-10-15 18:33:22 -0700

    finished

--- mp6/updateBoard.c
+++ mp6/updateBoard.c
@@ -1,4 +1,10 @@
-/*
+/* sasinkg2 akshay5 dhruvv2 */ 
+/* intro paragraph: 
+
+*/
+
+
+/* 
  * countLiveNeighbor
  * Inputs:
  * board: 1-D array of the current game board. 1 represents a live cell.
@@ -13,25 +19,22 @@
  */
 
 int countLiveNeighbor(int* board, int boardRowSize, int boardColSize, int row, int col){
-    int liveneighbor = 0; // initialize
-    for (int i = row - 1; i <= row + 1; i++) { 
-        if (i >= 0 && i < boardRowSize) {
-            for (int j = col - 1; j <= col + 1; j++) {
-                if (j >= 0 && j < boardColSize) {
-                    if (i != row || j != col) {
-                        if (board[i*boardColSize + j] ==1) {
-                            liveneighbor++;
-                        }
+int liveneighbors = 0; // initializing liveneighbors
+for(int i = row-1; i <=row+1; i++){ // checking neighboring rows
+    if(i>=0 && i<boardRowSize){ // within bounds
+        for(int j = col-1; j<=col+1; j++){ // checking neighboring columns
+            if(j>=0 && j<boardColSize){ // within bounds
+                if(i!=row || j!=col){ // if it's not the cell (row,col)
+                    if(board[i*boardColSize+j] == 1){ // is alive
+                        liveneighbors+=1; // increment liveneighbors
                     }
                 }
             }
         }
     }
-return liveneighbor;
-
 }
-
-
+    return liveneighbors;
+}
 /*
  * Update the game board to the next step.
  * Input: 
@@ -42,7 +45,29 @@ return liveneighbor;
  * Output: board is updated with new values for next step.
  */
 void updateBoard(int* board, int boardRowSize, int boardColSize) {
-}
+    int temp[boardRowSize*boardColSize]; // initializing temp array
+    for(int k = 0; k<boardRowSize; k++){
+       for(int l = 0; l<boardColSize; l++){
+        temp[k*boardColSize+l] = board[k*boardColSize+l]; // copying all values from board into temp
+    }}
+    for(int i = 0; i<boardRowSize; i++){ // changing values in temp if necessary
+        for(int j = 0; j<boardColSize; j++){
+            if(board[i*boardColSize+j] == 1){
+                if(countLiveNeighbor(board, boardRowSize, boardColSize, i, j)<2 || countLiveNeighbor(board, boardRowSize, boardColSize, i, j)>3){
+                    temp[i*boardColSize+j] = 0;
+                }
+            }
+            if(board[i*boardColSize+j] == 0){ // changing values in temp
+                if(countLiveNeighbor(board, boardRowSize, boardColSize, i, j) == 3){
+                    temp[i*boardColSize+j] = 1;
+                }
+            }
+        }
+    }
+    for(int k = 0; k<boardRowSize; k++){ 
+       for(int l = 0; l<boardColSize; l++){
+        board[k*boardColSize+l] = temp[k*boardColSize+l]; // copying temp to board
+    }}}
 
 /*
  * aliveStable
@@ -55,10 +80,38 @@ void updateBoard(int* board, int boardRowSize, int boardColSize) {
  * current step or there is no alive cells at all.
  * return 0 if the alive cells change for the next step.
  */ 
-// int aliveStable(int* board, int boardRowSize, int boardColSize){
-//}
+int aliveStable(int* board, int boardRowSize, int boardColSize){
+int temp[boardRowSize*boardColSize]; 
+for(int k = 0; k<boardRowSize; k++){ // copying board to temp
+    for(int l = 0; l<boardColSize; l++){
+        temp[k*boardColSize+l] = board[k*boardColSize+l];
+    }
+}
+for(int i = 0; i<boardRowSize; i++){ // changing values in temp as necessary
+    for(int j = 0; j<boardColSize; j++){
+        if(board[i*boardColSize+j] == 1){
+            if(countLiveNeighbor(board, boardRowSize, boardColSize, i, j)<2 || countLiveNeighbor(board, boardRowSize, boardColSize, i, j)>3){
+                temp[i*boardColSize+j] = 0;
+            }
+         }
+            if(board[i*boardColSize+j] == 0){ // changing values in temp
+                if(countLiveNeighbor(board, boardRowSize, boardColSize, i, j) == 3){
+                    temp[i*boardColSize+j] = 1;
+                }
+            }
+    }
+}
+for(int k = 0; k<boardRowSize; k++){ // comparing if there are changes in the new board (temp) and the original board
+    for(int l = 0; l<boardColSize; l++){
+        if(board[k*boardColSize+l] != temp[k*boardColSize+l]){
+            return 0;
+        }
+    }
+}
+    return 1;
+}
+                
+                
+            
 
-				
-				
-			
 
