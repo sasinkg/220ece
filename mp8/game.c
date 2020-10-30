760a26df8948488e257f43b82889c7bcc50465df
sasinkg2 <sasinkg2@illinois.edu>
2020-10-29 18:07:26 -0700

    test

--- mp8/game.c
+++ mp8/game.c
@@ -1,6 +1,5 @@
 #include "game.h"
 
-
 game * make_game(int rows, int cols)
 /*! Create an instance of a game structure with the given number of rows
     and columns, initializing elements to -1 and return a pointer
@@ -14,26 +13,37 @@ game * make_game(int rows, int cols)
     mygame->cells = malloc(rows*cols*sizeof(cell));
 
     //YOUR CODE STARTS HERE:  Initialize all other variables in game struct
-
-
+    mygame->cols = cols;
+    mygame->rows = rows;
+    mygame->score = 0;
+    int i = 0;
+    for(i=0; i<rows*cols; i++){
+        mygame->cells[i] = -1;
+    }
     return mygame;
 }
 
 void remake_game(game ** _cur_game_ptr,int new_rows,int new_cols)
 /*! Given a game structure that is passed by reference, change the
-	game structure to have the given number of rows and columns. Initialize
-	the score and all elements in the cells to -1. Make sure that any 
-	memory previously allocated is not lost in this function.	
+    game structure to have the given number of rows and columns. Initialize
+    the score and all elements in the cells to -1. Make sure that any 
+    memory previously allocated is not lost in this function.   
 */
 {
-	/*Frees dynamically allocated memory used by cells in previous game,
-	 then dynamically allocates memory for cells in new game.  DO NOT MODIFY.*/
-	free((*_cur_game_ptr)->cells);
-	(*_cur_game_ptr)->cells = malloc(new_rows*new_cols*sizeof(cell));
+    /*Frees dynamically allocated memory used by cells in previous game,
+     then dynamically allocates memory for cells in new game.  DO NOT MODIFY.*/
+    free((*_cur_game_ptr)->cells);
+    (*_cur_game_ptr)->cells = malloc(new_rows*new_cols*sizeof(cell));
 
-	 //YOUR CODE STARTS HERE:  Re-initialize all other variables in game struct
-
-	return;	
+     //YOUR CODE STARTS HERE:  Re-initialize all other variables in game struct
+    (*_cur_game_ptr)->cols = new_cols;
+    (*_cur_game_ptr)->rows = new_rows;
+    (*_cur_game_ptr)->score = 0;
+    int i = 0;
+    for(i=0; i<new_rows*new_cols; i++){
+        (*_cur_game_ptr)->cells[i] = -1;
+    }
+    return; 
 }
 
 void destroy_game(game * cur_game)
@@ -50,12 +60,16 @@ cell * get_cell(game * cur_game, int row, int col)
 /*! Given a game, a row, and a column, return a pointer to the corresponding
     cell on the game. (See game.h for game data structure specification)
     This function should be handy for accessing game cells. Return NULL
-	if the row and col coordinates do not exist.
+    if the row and col coordinates do not exist.
 */
 {
     //YOUR CODE STARTS HERE
-
-    return NULL;
+    int cellcol = cur_game->cols;
+    int cellrow = cur_game->rows;
+    if(cellcol<=col || col<0 || cellrow<=row || row<0){
+        return NULL;
+    }
+    return cur_game->cells + (row * cur_game->cols + col);
 }
 
 int move_w(game * cur_game)
@@ -67,41 +81,181 @@ int move_w(game * cur_game)
 */
 {
     //YOUR CODE STARTS HERE
-
-    return 1;
-};
-
+    int i = 0;
+    int j = 0;
+    int k = 0;
+    int validmove = 0;
+    int cols = cur_game->cols;
+    int rows = cur_game->rows;
+    for (j=0; j<cols; j++){
+        for(i=0; i<rows; i++){
+            if(cur_game->cells[i*cols+j] == -1){
+                continue;
+            }else{
+                for(k=i+1; k<rows; k++){
+                    if(cur_game->cells[k*cols+j] == -1){
+                        continue;
+                    }
+                    if(cur_game->cells[k*cols+j] == cur_game->cells[i*cols+j]){ //if the value in the first row and the row below match
+                        cur_game->cells[i*cols+j] = cur_game->cells[i*cols+j] + cur_game->cells[k*cols+j]; //set the first row to the sum
+                        cur_game->cells[k*cols+j] = -1; //set the row below to empty
+                        validmove = 1; //merge happened so this is a valid move
+                        cur_game->score += cur_game->cells[i*cols+j]; //add sum that was found before to score
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    for (i=0; i<rows; i++){
+        if(cur_game->cells[i*cols+j] == -1){
+            for(k=i+1; k<rows; k++){
+                if(cur_game->cells[i*cols+j] == -1 && cur_game->cells[k*cols+j != -1]){ //check that first row is empty and row below has a value
+                    cur_game->cells[i*cols+j] = cur_game->cells[k*cols+j];
+                    cur_game->cells[k*cols+j] = -1;
+                    validmove = 1;
+                }
+            }
+        }
+    }
+    return validmove;
+}
 int move_s(game * cur_game) //slide down
 {
     //YOUR CODE STARTS HERE
-
-    return 1;
-};
+    int i = 0;
+    int j = 0;
+    int k = 0;
+    int validmove = 0;
+    int cols = cur_game->cols;
+    int rows = cur_game->rows;
+    for (j=0; j<cols; j++){
+        for(i=rows-1; i>-0; i--){
+            if(cur_game->cells[i*cols+j] == -1){
+                continue;
+            }else{
+                for(k=i-1; k>=0; k++){
+                    if(cur_game->cells[k*cols+j] == cur_game->cells[i*cols+j]){ //if the value in the last row and the row above match
+                        cur_game->cells[i*cols+j] = cur_game->cells[i*cols+j] + cur_game->cells[k*cols+j]; //set the last row to the sum
+                        cur_game->cells[k*cols+j] = -1; //set the row above to empty
+                        validmove = 1; //merge happened so this is a valid move
+                        cur_game->score += cur_game->cells[i*cols+j]; //add sum that was found before to score
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    for (i=rows-1; i>=0; i++){
+        if(cur_game->cells[i*cols+j] == -1){
+            for(k=i-1; k>=0; k++){
+                if(cur_game->cells[i*cols+j] == -1 && cur_game->cells[k*cols+j != -1]){ //check that last row is empty and row above has a value
+                    cur_game->cells[i*cols+j] = cur_game->cells[k*cols+j];
+                    cur_game->cells[k*cols+j] = -1;
+                    validmove = 1;
+                }
+            }
+        }
+    }
+    return validmove;
+}
 
 int move_a(game * cur_game) //slide left
 {
     //YOUR CODE STARTS HERE
-
-    return 1;
-};
+    int i = 0;
+    int j = 0;
+    int k = 0;
+    int validmove = 0;
+    int cols = cur_game->cols;
+    int rows = cur_game->rows;
+    for (i=0; i<rows; i++){
+        for(j=0; j<cols; j++){
+            if(cur_game->cells[i*cols+j] == -1){
+                continue;
+            }else{
+                for(k=j+1; k<cols; k++){
+                    if(cur_game->cells[i*cols+k] == cur_game->cells[i*cols+j]){ //if the value in the first column and the next column match
+                        cur_game->cells[i*cols+j] = cur_game->cells[i*cols+j] + cur_game->cells[i*cols+k]; //set the first column to the sum
+                        cur_game->cells[i*cols+k] = -1; //set the next column to empty
+                        validmove = 1; //merge happened so this is a valid move
+                        cur_game->score += cur_game->cells[i*cols+j]; //add sum that was found before to score
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    for (j=0; j<cols; j++){
+        if(cur_game->cells[i*cols+j] == -1){
+            for(k=j+1; k<cols; k++){
+                if(cur_game->cells[i*cols+j] == -1 && cur_game->cells[i*cols+k != -1]){ //check that first column is empty and next column has a value
+                    cur_game->cells[i*cols+j] = cur_game->cells[i*cols+k];
+                    cur_game->cells[i*cols+k] = -1;
+                    validmove = 1;
+                }
+            }
+        }
+    }
+    return validmove;
+}
 
 int move_d(game * cur_game){ //slide to the right
     //YOUR CODE STARTS HERE
-
-    return 1;
-};
+    int i = 0;
+    int j = 0;
+    int k = 0;
+    int validmove = 0;
+    int cols = cur_game->cols;
+    int rows = cur_game->rows;
+    for (i=0; i<rows; i++){
+        for(j=cols-1; j>=0; j++){
+            if(cur_game->cells[i*cols+j] == -1){
+                continue;
+            }else{
+                for(k=j-1; k>=0; k--){
+                    if(cur_game->cells[i*cols+k] == cur_game->cells[i*cols+j]){ //if the value in the first column and the next column match
+                        cur_game->cells[i*cols+j] = cur_game->cells[i*cols+j] + cur_game->cells[i*cols+k]; //set the first column to the sum
+                        cur_game->cells[i*cols+k] = -1; //set the next column to empty
+                        validmove = 1; //merge happened so this is a valid move
+                        cur_game->score += cur_game->cells[i*cols+j]; //add sum that was found before to score
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    for (j=cols-1; j>=0; j--){
+        if(cur_game->cells[i*cols+j] == -1){
+            for(k=j-1; k>=0; k--){
+                if(cur_game->cells[i*cols+j] == -1 && cur_game->cells[i*cols+k != -1]){ //check that first column is empty and next column has a value
+                    cur_game->cells[i*cols+j] = cur_game->cells[i*cols+k];
+                    cur_game->cells[i*cols+k] = -1;
+                    validmove = 1;
+                }
+            }
+        }
+    }
+    return validmove;
+}
 
 int legal_move_check(game * cur_game)
 /*! Given the current game check if there are any legal moves on the board. There are
     no legal moves if sliding in any direction will not cause the game to change.
-	Return 1 if there are possible legal moves, 0 if there are none.
+    Return 1 if there are possible legal moves, 0 if there are none.
  */
 {
     //YOUR CODE STARTS HERE
-
+    game * k_game = make_game((*cur_game).rows,(*cur_game).cols); //copy current game to a korary game
+    int i = 0;
+    for(i=0; i<(cur_game->rows)*(cur_game->cols); i++){
+        k_game->cells[i] = cur_game->cells[i]; //copy each cell
+    }
+    if(move_w(k_game) == 0 && move_s(k_game) == 0 && move_a(k_game) == 0 && move_d(k_game) == 0){
+        return 0; //if all move functions return 0, there is no legal move so return 0
+    }
     return 1;
 }
-
 
 /*! code below is provided and should not be changed */
 
@@ -109,69 +263,69 @@ void rand_new_tile(game * cur_game)
 /*! insert a new tile into a random empty cell. First call rand()%(rows*cols) to get a random value between 0 and (rows*cols)-1.
 */
 {
-	
-	cell * cell_ptr;
-    cell_ptr = 	cur_game->cells;
-	
-    if (cell_ptr == NULL){ 	
+    
+    cell * cell_ptr;
+    cell_ptr =  cur_game->cells;
+    
+    if (cell_ptr == NULL){  
         printf("Bad Cell Pointer.\n");
         exit(0);
     }
-	
-	
-	//check for an empty cell
-	int emptycheck = 0;
-	int i;
-	
-	for(i = 0; i < ((cur_game->rows)*(cur_game->cols)); i++){
-		if ((*cell_ptr) == -1){
-				emptycheck = 1;
-				break;
-		}		
+    
+    
+    //check for an empty cell
+    int emptycheck = 0;
+    int i;
+    
+    for(i = 0; i < ((cur_game->rows)*(cur_game->cols)); i++){
+        if ((*cell_ptr) == -1){
+                emptycheck = 1;
+                break;
+        }       
         cell_ptr += 1;
-	}
-	if (emptycheck == 0){
-		printf("Error: Trying to insert into no a board with no empty cell. The function rand_new_tile() should only be called after tiles have succesfully moved, meaning there should be at least 1 open spot.\n");
-		exit(0);
-	}
-	
+    }
+    if (emptycheck == 0){
+        printf("Error: Trying to insert into no a board with no empty cell. The function rand_new_tile() should only be called after tiles have succesfully moved, meaning there should be at least 1 open spot.\n");
+        exit(0);
+    }
+    
     int ind,row,col;
-	int num;
+    int num;
     do{
-		ind = rand()%((cur_game->rows)*(cur_game->cols));
-		col = ind%(cur_game->cols);
-		row = ind/cur_game->cols;
+        ind = rand()%((cur_game->rows)*(cur_game->cols));
+        col = ind%(cur_game->cols);
+        row = ind/cur_game->cols;
     } while ( *get_cell(cur_game, row, col) != -1);
         //*get_cell(cur_game, row, col) = 2;
-	num = rand()%20;
-	if(num <= 1){
-		*get_cell(cur_game, row, col) = 4; // 1/10th chance
-	}
-	else{
-		*get_cell(cur_game, row, col) = 2;// 9/10th chance
-	}
+    num = rand()%20;
+    if(num <= 1){
+        *get_cell(cur_game, row, col) = 4; // 1/10th chance
+    }
+    else{
+        *get_cell(cur_game, row, col) = 2;// 9/10th chance
+    }
 }
 
 int print_game(game * cur_game) 
 {
     cell * cell_ptr;
-    cell_ptr = 	cur_game->cells;
+    cell_ptr =  cur_game->cells;
 
     int rows = cur_game->rows;
     int cols = cur_game->cols;
     int i,j;
-	
-	printf("\n\n\nscore:%d\n",cur_game->score); 
-	
-	
-	printf("\u2554"); // topleft box char
-	for(i = 0; i < cols*5;i++)
-		printf("\u2550"); // top box char
-	printf("\u2557\n"); //top right char 
-	
-	
+    
+    printf("\n\n\nscore:%d\n",cur_game->score); 
+    
+    
+    printf("\u2554"); // topleft box char
+    for(i = 0; i < cols*5;i++)
+        printf("\u2550"); // top box char
+    printf("\u2557\n"); //top right char 
+    
+    
     for(i = 0; i < rows; i++){
-		printf("\u2551"); // side box char
+        printf("\u2551"); // side box char
         for(j = 0; j < cols; j++){
             if ((*cell_ptr) == -1 ) { //print asterisks
                 printf(" **  "); 
@@ -217,32 +371,32 @@ int print_game(game * cur_game)
                     case 8192:
                         printf("\x1b[31m%04d\x1b[0m ",(*cell_ptr));
                         break;
-					default:
-						printf("  X  ");
+                    default:
+                        printf("  X  ");
 
                 }
 
             }
             cell_ptr++;
         }
-	printf("\u2551\n"); //print right wall and newline
+    printf("\u2551\n"); //print right wall and newline
     }
-	
-	printf("\u255A"); // print bottom left char
-	for(i = 0; i < cols*5;i++)
-		printf("\u2550"); // bottom char
-	printf("\u255D\n"); //bottom right char
-	
+    
+    printf("\u255A"); // print bottom left char
+    for(i = 0; i < cols*5;i++)
+        printf("\u2550"); // bottom char
+    printf("\u255D\n"); //bottom right char
+    
     return 0;
 }
 
 int process_turn(const char input_char, game* cur_game) //returns 1 if legal move is possible after input is processed
 { 
-	int rows,cols;
-	char buf[200];
-	char garbage[2];
+    int rows,cols;
+    char buf[200];
+    char garbage[2];
     int move_success = 0;
-	
+    
     switch ( input_char ) {
     case 'w':
         move_success = move_w(cur_game);
@@ -261,34 +415,34 @@ int process_turn(const char input_char, game* cur_game) //returns 1 if legal mov
         printf("\nQuitting..\n");
         return 0;
         break;
-	case 'n':
-		//get row and col input for new game
-		dim_prompt: printf("NEW GAME: Enter dimensions (rows columns):");
-		while (NULL == fgets(buf,200,stdin)) {
-			printf("\nProgram Terminated.\n");
-			return 0;
-		}
-		
-		if (2 != sscanf(buf,"%d%d%1s",&rows,&cols,garbage) ||
-		rows < 0 || cols < 0){
-			printf("Invalid dimensions.\n");
-			goto dim_prompt;
-		} 
-		
-		remake_game(&cur_game,rows,cols);
-		
-		move_success = 1;
-		
+    case 'n':
+        //get row and col input for new game
+        dim_prompt: printf("NEW GAME: Enter dimensions (rows columns):");
+        while (NULL == fgets(buf,200,stdin)) {
+            printf("\nProgram Terminated.\n");
+            return 0;
+        }
+        
+        if (2 != sscanf(buf,"%d%d%1s",&rows,&cols,garbage) ||
+        rows < 0 || cols < 0){
+            printf("Invalid dimensions.\n");
+            goto dim_prompt;
+        } 
+        
+        remake_game(&cur_game,rows,cols);
+        
+        move_success = 1;
+        
     default: //any other input
         printf("Invalid Input. Valid inputs are: w, a, s, d, q, n.\n");
     }
 
-	
-	
-	
+    
+    
+    
     if(move_success == 1){ //if movement happened, insert new tile and print the game.
          rand_new_tile(cur_game); 
-		 print_game(cur_game);
+         print_game(cur_game);
     } 
 
     if( legal_move_check(cur_game) == 0){  //check if the newly spawned tile results in game over.
@@ -297,3 +451,5 @@ int process_turn(const char input_char, game* cur_game) //returns 1 if legal mov
     }
     return 1;
 }
+
+
