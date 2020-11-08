72b7681169c60d0653f5c159874e6d9b27f7386d
sasinkg2 <sasinkg2@illinois.edu>
2020-11-07 21:30:47 -0800

    finished (wifi issues - written in file)

--- mp9/maze.c
+++ mp9/maze.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "maze.h"
-/* partners sasinkg2 akshay5 dhruvv2 */
+
+/* this is late because of internet issues - my wifi at home went out for a few hours and 
+it ended up being something wrong with our internet plan. 
+I know its not an excuseable circumstance but because the midterm was on Thursday I didnt 
+get an opportunity to start this MP until after, and when we did finish, I lost connection while
+working on it so I wasn't able to submit it on time */ 
+
+/* we allocated memory for the board by reading the file and creating a new array.
+ * then we create destroy and print functions so that the board prints and then destroys
+ * at the very end. we then use recursive calls to check each move (up, down, left, right)
+ * to find the correct move and then we make that move. we had trouble with the solve
+ * function because we kept having segmentation faults since part of our logic was wrong
+ * and it was accessing memory that didn't exist.
+ * group memmbers: sasinkg2, dhruvv2, akshay5
+ */
 /*
  * createMaze -- Creates and fills a maze structure from the given file
  * INPUTS:       fileName - character array containing the name of the maze file
@@ -20,21 +34,18 @@ maze_t * createMaze(char * fileName)
         return NULL;
     }
     fscanf(fptr, "%d %d", &rows, &cols); //scan for row and col size
-    
-    
-    maze_t * maze = (maze_t *)wmalloc(sizeof(maze_t)); //create space for maze structure
-    maze->cells=(char **)malloc((maze->height)*sizeof(char*)); //create space for lookup
-    maze->startRow = -1;
-    maze->startColumn = -1;
-    maze->endRow = -1;
-    maze->endColumn = -1;
-    for(i=0;i<maze->height;i++){
-      maze->cells[i]=(char*)malloc(sizeof(char)*(maze->width)); //fill for columns
+    maze_t *maze = (maze_t *)malloc(sizeof(maze_t)); //create space for maze structure
+    maze->cells=(char**)malloc((rows)*sizeof(char*)); //create space for lookup
+    maze->height = rows;
+    maze->width = cols; //initialize maze structure 
+    for(i=0;i<rows;i++){
+      maze->cells[i]=(char*)malloc(sizeof(char)*(cols)); //fill for columns
     }
     char temp;
     for(i=0; i<rows; i++){
         for(j=0; j<cols; j++){
-            fscanf (fptr, "%c", &temp); //read character
+            temp = fgetc(fptr); //read character
+            if(i==10) printf("%c ", temp);
             if(temp=='\n'){
                 j--;
             }else{
@@ -43,15 +54,17 @@ maze_t * createMaze(char * fileName)
             if(temp=='S'){
                 maze->startRow = i;
                 maze->startColumn = j;
+                
             }
             if(temp=='E'){
                 maze->endRow = i;
                 maze->endColumn = j;
+                
             }
         }
     }
     fclose(fptr);
-    return maze;
+    return maze; 
 }
 
 /*
@@ -86,9 +99,11 @@ void printMaze(maze_t * maze)
     int j;
     for(i=0; i<maze->height; i++){
         for (j=0; j<maze->width; j++){ //loop through board and print
+            
             printf("%c", maze->cells[i][j]);
         }
         printf("\n"); //print newlines to make board look nice
+        
     }
 }
 
@@ -103,22 +118,21 @@ void printMaze(maze_t * maze)
  */ 
 int solveMazeDFS(maze_t * maze, int col, int row)
 {
-    if(maze->cells[row][col] == 'E'){ //check if end has been reached
-        maze->cells[maze->startRow][maze->startColumn] = 'S'; //set back to start
-        return 1;
-    }
     if(col<0 || row<0 || col>=maze->width || row>=maze->height){ //check boundary conditions
         return 0;
     }
-    if(maze->cells[row][col]!=' ' && maze->cells[row][col]!='%' && maze->cells[row][col]!='S'){
-        return 0;
+    if(maze->cells[row][col] == 'E'){ //check if end has been reached
+        maze->cells[maze->startRow][maze->startColumn] = 'S'; //set back to start
+        return 1;
+    } 
+    if(maze->cells[row][col]==' ' || maze->cells[row][col] == 'S'){ //if not valid character
+        maze->cells[row][col] = '*'; //correct solution if empty conditions are met
+        if(solveMazeDFS(maze, col, row+1) == 1) return 1;
+        if(solveMazeDFS(maze, col, row-1) == 1) return 1;
+        if(solveMazeDFS(maze, col+1, row) == 1) return 1;
+        if(solveMazeDFS(maze, col-1, row) == 1) return 1; //check down, up, right, left for a correct move through a recursive call
+        maze->cells[row][col]='%'; //mark currently found path as wall so as to not go backwards
     }
-    maze->cells[row][col] = '*'; //correct solution if empty conditions are met
-    if(solveMazeDFS(maze, col, row+1)) return 1;
-    if(solveMazeDFS(maze, col, row-1)) return 1;
-    if(solveMazeDFS(maze, col+1, row)) return 1;
-    if(solveMazeDFS(maze, col-1, row)) return 1; //check down, up, right, left for a correct move through a recursive call
-    maze->cells[row][col]='%'; //mark currently found path as wall so as to not go backwards
     return 0;
 }
 
