5a2646a4b3a27435ab99e36c06c64326b272417d
sasinkg2 <sasinkg2@illinois.edu>
2020-12-05 21:32:48 -0600

    fixed

--- mp12/ImaginaryNumber.h
+++ mp12/ImaginaryNumber.h
@@ -1,139 +1,52 @@
-#include "ImaginaryNumber.h"
-#include "helper.h"
+#ifndef IMAGINARY_NUMBER_H
+#define IMAGINARY_NUMBER_H
 
-ImaginaryNumber::ImaginaryNumber()
-{
-    /* Your code here */
-    imaginary_component = 0;
-    magnitude = 0;
-    phase = 0;
-    number_type = IMAGINARY;
-}
+#include "Number.h"
+class ImaginaryNumber;  //This is a forward declaration, needed only to remove circular dependencies
+#include "RealNumber.h"
+#include "ComplexNumber.h"
+#include <stdlib.h>
 
-ImaginaryNumber::ImaginaryNumber(double rval)
-{
-    /* Your code here */
-    imaginary_component = rval;
-    magnitude = abs(rval);
-    phase = calculate_phase(0, rval);
-    number_type = IMAGINARY;
-}
+class ImaginaryNumber:public Number{
+    private:
+        //Class variables
+        double imaginary_component;
+    public:
+        //Empty Constructor     
+        ImaginaryNumber();
+        //Complete Constructor
+        ImaginaryNumber(double rval);
+        //Copy Constructor
+        ImaginaryNumber(const ImaginaryNumber&);
 
-ImaginaryNumber::ImaginaryNumber( const ImaginaryNumber& other )
-{
-    /* Your code here */
-    imaginary_component = other.get_imaginary_component();
-    magnitude = other.get_magnitude();
-    phase = other.get_phase();
-    number_type = other.get_number_type();
-}
+        //Setter and getter functions
+        void set_imaginary_component(double rval);
+        double get_imaginary_component() const; 
+        double get_magnitude() const;   
+        double get_phase() const;
 
-void ImaginaryNumber::set_imaginary_component (double rval)
-{
-    /* Your code here */
-    imaginary_component = rval;
-    magnitude = abs(rval);
-    phase = calculate_phase(0, rval);
-}
+        //Operation overload for ImaginaryNumber (op) ImaginaryNumber
+        ImaginaryNumber operator + (const ImaginaryNumber& arg);
+        ImaginaryNumber operator - (const ImaginaryNumber& arg);
+        RealNumber operator * (const ImaginaryNumber& arg);
+        RealNumber operator / (const ImaginaryNumber& arg);
 
-double ImaginaryNumber::get_imaginary_component() const
-{
-    /* Your code here */
-    return imaginary_component;
-}
+        //Operation overload for ImaginaryNumber (op) RealNumber
+        ComplexNumber operator + (const RealNumber& arg);
+        ComplexNumber operator - (const RealNumber& arg);
+        ImaginaryNumber operator * (const RealNumber& arg);
+        ImaginaryNumber operator / (const RealNumber& arg);
 
-//Getter function for magnitude
-double ImaginaryNumber::get_magnitude() const{
-    /* Your code here */
-    return magnitude;
-}
+        //Operation overload for ImaginaryNumber (op) ComplexNumber
+        ComplexNumber operator + (const ComplexNumber& arg);
+        ComplexNumber operator - (const ComplexNumber& arg);
+        ComplexNumber operator * (const ComplexNumber& arg);
+        ComplexNumber operator / (const ComplexNumber& arg);
 
-//Getter function for phase
-double ImaginaryNumber::get_phase() const{
-    /* Your code here */
-    return phase;
-}
+        //Output class variables as string (provided)
+        string to_String();
+};
 
-//Operator Overload
-ImaginaryNumber ImaginaryNumber::operator + (const ImaginaryNumber& arg)
-{
-    /* Your code here */
-    return ImaginaryNumber(imaginary_component + arg.get_imaginary_component());
-}
+#endif
 
-ImaginaryNumber ImaginaryNumber::operator - (const ImaginaryNumber& arg)
-{
-    /* Your code here */
-    return ImaginaryNumber(imaginary_component - arg.get_imaginary_component());
-}
-
-RealNumber ImaginaryNumber::operator * (const ImaginaryNumber& arg)
-{
-    /* Your code here */
-    return RealNumber(-1 * imaginary_component * arg.get_imaginary_component());
-}
-
-RealNumber ImaginaryNumber::operator / (const ImaginaryNumber& arg)
-{
-    /* Your code here */
-    return RealNumber(imaginary_component / arg.get_imaginary_component());
-}
-
-ComplexNumber ImaginaryNumber::operator + (const RealNumber& arg)
-{
-    /* Your code here */
-    return ComplexNumber(arg.get_real_component(), + imaginary_component);
-}
-
-ComplexNumber ImaginaryNumber::operator - (const RealNumber& arg)
-{
-    /* Your code here */
-    return ComplexNumber(-1 * arg.get_real_component(), + imaginary_component);
-}
-
-ImaginaryNumber ImaginaryNumber::operator * (const RealNumber& arg)
-{
-    /* Your code here */
-    return ImaginaryNumber(arg.get_real_component() * imaginary_component);
-}
-
-ImaginaryNumber ImaginaryNumber::operator / (const RealNumber& arg)
-{
-    /* Your code here */
-    return ImaginaryNumber(imaginary_component / arg.get_real_component());
-}
-
-ComplexNumber ImaginaryNumber::operator + (const ComplexNumber& arg)
-{
-    /* Your code here */
-    return ComplexNumber(arg.get_real_component(), + imaginary_component + arg.get_imaginary_component());
-}
-
-ComplexNumber ImaginaryNumber::operator - (const ComplexNumber& arg)
-{
-    /* Your code here */
-    return ComplexNumber(-1 * arg.get_real_component(), + imaginary_component - arg.get_imaginary_component());
-}
-
-ComplexNumber ImaginaryNumber::operator * (const ComplexNumber& arg)
-{
-    /* Your code here */
-    return ComplexNumber(-1 * imaginary_component * arg.get_imaginary_component(), imaginary_component * arg.get_real_component());
-}
-
-ComplexNumber ImaginaryNumber::operator / (const ComplexNumber& arg)
-{
-    /* Your code here */
-    double den = (arg.get_real_component()*arg.get_real_component() + arg.get_imaginary_component()*arg.get_imaginary_component());
-    double num_imag = (imaginary_component*arg.get_real_component());
-    double num_real = (imaginary_component*arg.get_imaginary_component());
-    return ComplexNumber(num_real / den , num_imag / den);
-}
-
-string ImaginaryNumber::to_String(){
-    /* Do not modify */
-    stringstream my_output;
-    my_output << std::setprecision(3) << imaginary_component << 'i';
-    return my_output.str();
-}
 
